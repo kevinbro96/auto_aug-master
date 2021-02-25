88ef7a79c3b4b3c65d62e40112c203fd8ff9c116
kevinbro96 <69466636+kevinbro96@users.noreply.github.com>
2021-02-25 23:55:26 +0800

    Update vae.py

--- aa/networks/vae.py
+++ aa/networks/vae.py
@@ -10,6 +10,7 @@ import torch.utils.data
 from torch import nn
 from torch.nn import init
 from torch.nn import functional as F
+from torch.autograd import Variable
 
 def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=True)
